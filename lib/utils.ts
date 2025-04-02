153a462c3f3fbd6fe9fb76c52b35db5513101b57
lighteko <dev.lighteko@gmail.com>
2025-04-02 17:39:04 -0400

    EDIT: Cron Conversion method modified

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,5 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export function weeklyCron(weekday: string) {
   const weekdays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-  return `0 8 * * ${weekdays.indexOf(weekday)}`;
+  const dayOfWeek = weekdays.indexOf(weekday.toUpperCase());
+  if (dayOfWeek === -1) throw new Error("Invalid weekday");
+  return `cron(0 8 ? * ${dayOfWeek} *)`;
 }
